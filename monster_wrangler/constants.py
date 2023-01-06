9f337aa9f1e905f6c663e2d3c56ba94b024ef8a2
Kamal <kamal.farahani90@gmail.com>
2023-01-06 12:37:39 +0330

    add constants

---
+++ monster_wrangler/constants.py
@@ -0,0 +1,43 @@
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 800
+
+FONT_PATH = './assets/Abrushow.ttf'
+FONT_SIZE = 32
+
+TITLE_STR = 'Monster Wrangler'
+SCORE_STR = 'Score'
+LIVES_STR = 'Lives'
+CURRENT_ROUND_STR = 'Current round'
+CURRENT_CATCH_STR = 'Current catch'
+ROUND_TIME_STR = 'Round time'
+WARPS_STR = 'Warps'
+GAME_OVER_STR = 'Game over!'
+
+
+CATCH_SOUND_PATH = './assets/catch.wav'
+DIE_SOUND_PATH = './assets/die.wav'
+WARP_SOUND_PATH = './assets/warp.wav'
+NEXT_LEVEL_SOUND_PATH = './assets/next_level.wav'
+
+KINIGHT_IMG_PATH = './assets/knight.png'
+BLUE_MONSTER_IMG_PATH = './assets/blue_monster.png'
+YELLOW_MONSTER_IMG_PATH = './assets/yellow_monster.png'
+GREEN_MONSTER_IMG_PATH = './assets/green_monster.png'
+PURPLE_MONSTER_IMG_PATH = './assets/purple_monster.png'
+
+INIT_LIVES = 3
+INIT_WARPS = 3
+INIT_MONSTERS_COUNT = 4
+INIT_MONSTER_VELOCITY = 5
+INIT_PLAYER_VELOCITY = 8
+MONSTER_GROWTH_NUMBER = 3
+
+TOP_MARGIN = 200
+BOTTOM_MARGIN = 150
+LEFT_MARGIN = 10
+
+ORANGE = (246, 170, 54)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+FPS = 40
